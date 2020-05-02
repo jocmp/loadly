901baacf1c235bc5019469627e3e68b6b764f94c
Josiah Campbell <jocmp64@gmail.com>
2020-05-02 03:13:59 -0500

    Re-configure routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'home#index'
+  root "home#index"
 
-  get '/', to: 'home#index', as: 'home'
-  get '/detail', to: 'home#detail', as: 'detail'
+  get :detail, to: "home#detail", as: :detail
+  resources :home, path: "/", only: [:index]
 end
